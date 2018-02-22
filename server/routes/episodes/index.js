0cf713163f79e63b155eca9cc9c858853578640f
steve <bashcandicoot@gmail.com>
2018-02-22 15:53:24 +0000

    updated urls

--- server/routes/episodes/index.js
+++ server/routes/episodes/index.js
@@ -2,7 +2,7 @@ const episodes = require('express').Router();
 const all = require('./all');
 const season = require('./season');
 
-episodes.get('/', all);
-episodes.get('/:season', season);
+episodes.get('/silicon-valley', all);
+episodes.get('/silicon-valley/season/:season', season);
 
 module.exports = episodes;
