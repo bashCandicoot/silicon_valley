ca6e72e87fbb67dd50b3f006b656db47469186c4
steve <bashcandicoot@gmail.com>
2018-02-22 14:56:16 +0000

    replaced HelloWorld with Episodes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Episodes from '@/components/Episodes';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      path: '/silicon-valley',
+      name: 'silicon-valley',
+      component: Episodes,
     },
   ],
 });
